53ace20e311ac7397d6e362a71cab34f2d29f9ef
Arnold Chand <creativenull@outlook.com>
2021-06-13 21:53:08 -0400

    feat(init): formatting

--- init.lua
+++ init.lua
@@ -1,18 +1,41 @@
+-- DO NOT DO THIS, check the link
+-- https://dev.to/creativenull/installing-neovim-nightly-alongside-stable-10d0
+-- Runtime Path
+vim.cmd 'set runtimepath-=~/.config/nvim'
+vim.cmd 'set runtimepath-=~/.config/nvim/after'
+vim.cmd 'set runtimepath-=~/.local/share/nvim/site'
+vim.cmd 'set runtimepath-=~/.local/share/nvim/site/after'
+vim.cmd 'set runtimepath-=/etc/xdg/nvim'
+vim.cmd 'set runtimepath-=/etc/xdg/nvim/after'
+vim.cmd 'set runtimepath-=/usr/share/nvim/site'
+vim.cmd 'set runtimepath-=/usr/share/nvim/site/after'
+vim.cmd 'set runtimepath-=/usr/local/share/nvim/site'
+vim.cmd 'set runtimepath-=/usr/local/share/nvim/site/after'
+
+vim.cmd 'set runtimepath+=~/.config/nvim-aula/after'
+vim.cmd 'set runtimepath^=~/.config/nvim-aula'
+vim.cmd 'set runtimepath+=~/.local/share/nvim-aula/site/after'
+vim.cmd 'set runtimepath^=~/.local/share/nvim-aula/site'
+
+-- Pack Path
+vim.cmd 'set packpath-=~/.config/nvim'
+vim.cmd 'set packpath-=~/.config/nvim/after'
+vim.cmd 'set packpath-=~/.local/share/nvim/site'
+vim.cmd 'set packpath-=~/.local/share/nvim/site/after'
+vim.cmd 'set packpath-=/etc/xdg/nvim'
+vim.cmd 'set packpath-=/etc/xdg/nvim/after'
+vim.cmd 'set packpath-=/usr/local/share/nvim/site'
+vim.cmd 'set packpath-=/usr/local/share/nvim/site/after'
+vim.cmd 'set packpath-=/usr/share/nvim/site'
+vim.cmd 'set packpath-=/usr/share/nvim/site/after'
+
+vim.cmd 'set packpath^=~/.config/nvim-aula'
+vim.cmd 'set packpath+=~/.config/nvim-aula/after'
+vim.cmd 'set packpath^=~/.local/share/nvim-aula/site'
+vim.cmd 'set packpath+=~/.local/share/nvim-aula/site/after'
+
 -- =============================================================================
--- = Core =
+-- = Aula Initial Start =
 -- =============================================================================
-
-local option = require('./option')
--- local Keymap = require('./Keymap')
--- local Event = require('./Event')
-
--- Option.set('spelloptions', 'camel')
--- Option.setGroup({
---   { 'spell', false },
---   { 'softtabstop', 2 }
--- })
-
--- Event:init()
--- Event:set({ event = 'WinEnter', cmd = ':echom "Hello from vimL"' })
--- Event:set({ event = 'InsertEnter', cmd = function() print('Hello from lua') end })
--- Event:setup()
+local aula = require 'aula'
+aula.init()
