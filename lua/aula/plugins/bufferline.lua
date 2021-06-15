714ab9330ebc2918a3f913a41c9e6ab9ee72d22c
Arnold Chand <creativenull@outlook.com>
2021-06-15 18:36:03 -0400

    feat(plugins): bufferline updates, use nnn lua setup()

--- lua/aula/plugins/bufferline.lua
+++ lua/aula/plugins/bufferline.lua
@@ -8,9 +8,6 @@ end
 function M.setup()
   require 'bufferline'.setup {
     options = {
-      indicator_icon = '',
-      close_icon = '',
-      buffer_close_icon = '',
       show_buffer_close_icons = false,
       show_close_icon = false
     }
--- lua/aula/plugins/nnn.lua
+++ lua/aula/plugins/nnn.lua
@@ -7,13 +7,18 @@ function M.init()
   pkg.add('mcchrish/nnn.vim')
 end
 
--- Set options before loading plugin
-function M.config()
-  vim.g['nnn#layout'] = { right = '~25%' }
-end
-
 -- Set options after loading plugin
 function M.setup()
+  require 'nnn'.setup {
+    layout = {
+      window = {
+        width = 0.9,
+        height = 0.6,
+        highlight = 'Debug'
+      }
+    }
+  }
+
   keymap.set('n', '<F3>', '<cmd>Np<CR>')
 end
 
