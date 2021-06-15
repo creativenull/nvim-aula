f403a7b6898df6996e8b7b814f44c9080f0ad0a6
Arnold Chand <creativenull@outlook.com>
2021-06-15 10:33:37 -0400

    feat(init): add global config

--- lua/aula/config.lua
+++ lua/aula/config.lua
@@ -4,9 +4,43 @@ local command = require 'aula.core.command'
 local reload = require 'aula.core.vim.reload'
 local pkg = require 'aula.core.package'
 local theme = require 'aula.core.theme'
+local templates = require 'aula.core.templates'
 local M = {}
 
 function M.start()
+  local namespace = 'aula'
+
+  _G.aula = {
+    debug = false,
+
+    config = {
+      templates_dir = string.format('%s/.config/nvim-aula/lua/%s/core/templates', vim.env.HOME, namespace),
+      plugins_dir = string.format('%s/.config/nvim-aula/lua/%s/plugins', vim.env.HOME, namespace)
+    },
+
+    theme = {},
+
+    commands = {
+      set = {},
+      queue = {}
+    },
+
+    keymaps = {
+      set = {},
+      queue = {}
+    },
+
+    events = {
+      set = {},
+      queue = {}
+    },
+
+    package = {
+      plugin_deps = {},
+      plugins = {}
+    }
+  }
+
   keymap.init({ leader = ' ', localleader = ',' })
   event.init()
   pkg.init()
@@ -15,6 +49,7 @@ end
 function M.finish()
   theme.setup()
   reload.setup()
+  templates.setup()
   command.setup()
   keymap.setup()
   event.setup()
--- lua/aula/init.lua
+++ lua/aula/init.lua
@@ -1,35 +1,10 @@
+local config = require 'aula.config'
 local M = {}
 
-_G.aula = {
-  debug = false,
-
-  theme = {},
-
-  commands = {
-    set = {},
-    queue = {}
-  },
-
-  keymaps = {
-    set = {},
-    queue = {}
-  },
-
-  events = {
-    set = {},
-    queue = {}
-  },
-
-  package = {
-    plugin_deps = {},
-    plugins = {}
-  }
-}
-
 function M.init()
-  require 'aula.config'.start()
+  config.start()
   require 'aula.user'
-  require 'aula.config'.finish()
+  config.finish()
 end
 
 return M
